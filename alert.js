4833f0d581af091cdc5def92b679e1cd94651043
Munnass95 <mehedihasan162120@gmail.com>
2021-09-05 23:47:18 +0600

    first commit

---
+++ alert.js
@@ -0,0 +1,21 @@
+// alert("Ma is coming!! Open the book ");
+
+const maComing = () => {
+  alert("Ammu is coming. Open the book");
+};
+
+const picnicJabi = () => {
+  const response = confirm("picnic jabi?");
+  console.log(response);
+
+  if (response === true) {
+    alert("Amake fee ta bkash kore dee");
+  } else {
+    console.log("Dure giya mor hala");
+  }
+};
+
+const askName = () => {
+  const name = prompt("what is your name");
+  console.log(name);
+};
